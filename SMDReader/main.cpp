5f6d22827e48a0baee86398d6c539930d91756ab
Buster Schrader <bstrdash@gmail.com>
2017-04-12 12:05:08 -0500

    Made absolute reference relative.

--- SMDReader/main.cpp
+++ SMDReader/main.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main(int argc, char* argv[])
 {
-	smd_model_reader* smr = new smd_model_reader("C:\\users\\buster\\desktop\\smdreader\\cube.smd");
+	smd_model_reader* smr = new smd_model_reader("cube.smd");
 	
 	delete smr;
 	return 0;	
