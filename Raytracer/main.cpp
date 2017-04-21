a8fe2938c054021d816fc2bcfe102d91dcc3b03e
Buster <bstrdash@gmail.com>
2017-04-21 12:20:12 -0500

    Forgot what I changed...

--- Raytracer/main.cpp
+++ Raytracer/main.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
+#include <cmath>
+#include <string>
+#include <iomanip>
+#include <sys/time.h>
+
 #include "raytracer.hpp"
 #include "windows_bitmap.hpp"
 #include "smd_model_reader.hpp"
-#include <cmath>
 
 int main(int argc, char* argv[])
 {
@@ -11,16 +15,16 @@ int main(int argc, char* argv[])
 
 	std::cout << smr->get_triangle_count() << std::endl;
 	
-	float pitch = 48.8f - 90.0f;
-	float yaw = 40.0f + 180.0f;
+	float pitch = 30.0f - 90.0f;
+	float yaw = 15.0f + 180.0f;
 	float const PI = 3.141592f;
 	float sx = cos(yaw*PI/180) * cos(pitch*PI/180);
 	float sy = sin(yaw*PI/180) * cos(pitch*PI/180);
 	float sz = sin(pitch*PI/180);
 	std::cout << sx << ' ' << sy << ' ' << sz << std::endl;
-	vertex* sun = new vertex(1.5f,0.5f,2.5f,sx,sy,sz,0.0f,0.0f);
+	vertex* sun = new vertex(1.0f,0.0f,4.0f,sx,sy,sz,0.0f,0.0f);
 	vertex* hit = new vertex();
-	
+
 	for (unsigned int i = 0; i < smr->get_triangle_count(); i++)
 	{
 		bool rayhit = raytracer::get_intersection(sun, smr->get_triangle(i), hit);
@@ -38,7 +42,7 @@ int main(int argc, char* argv[])
 		
 		wb->get_dib()->get_image()->set_pixel((unsigned int)round(hit->u*128.0f),(unsigned int)round(128.0f-hit->v*128.0f), 0x000000);
 	}
-
+	
 	wb->save();
 	
 	delete wb;
